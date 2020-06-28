6a6717dc1aaa62d14beadfe1477acd7045c881d3
Ori Naftaly <naftalyblues@gmail.com>
2020-06-28 13:11:10 -0500

    Updates

---
+++ precache-manifest.7789a980e41d2e168ed53e8ae550a6cb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382c2c6e185eeab43c8ec321acda62d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8d5bfdb5ea5fbaf32263",
+    "url": "/static/css/main.4d6722d4.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d5bfdb5ea5fbaf32263",
+    "url": "/static/js/main.6707fd24.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
